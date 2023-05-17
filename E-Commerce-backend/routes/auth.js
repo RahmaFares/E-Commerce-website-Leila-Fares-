d34df72f32d84ac30f0f427ac94b28792ef59a0c
Rahma Fares <rwalid.nes@gmail.com>
2023-05-18 01:52:23 +0200

    Authorization + UPDATE/DELETE/GET methods

--- E-Commerce-backend/routes/auth.js
+++ E-Commerce-backend/routes/auth.js
@@ -1,13 +1,15 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
 //REGISTER FUNCTION 
 router.post("/register", async (req, res) => {
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
-        password: CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString(),
+        password: CryptoJS.AES.encrypt(
+            req.body.password, process.env.PASS_SEC).toString(),
         //we need to encrypt the password, I will use CryptoJS
     });
     try {
@@ -34,8 +36,17 @@ router.post("/login", async (req, res) => {
         Original_password !== req.body.password &&
             res.status(401).json("Wrong Credentials!");
 
+        //creating json webtoken 
+        const accessToken = jwt.sign({
+            id: user._id,
+            isAdmin: user.isAdmin,
+
+        }, process.env.JWT_SEC_KEY,
+            { expiresIn: "3d" }
+        );
+
         const { password, ...others } = user._doc;
-        res.status(200).json(others);
+        res.status(200).json({ ...others, accessToken });
     } catch (err) {
         res.status(500).json(err);
     }
--- E-Commerce-backend/routes/user.js
+++ E-Commerce-backend/routes/user.js
@@ -1,4 +1,24 @@
 const router = require("express").Router();
+const { verifyToken, verifyToken_andAuth } = require("./verifyToken");
 
+//put because we are UPDATING
+router.put("/:id", verifyToken_andAuth, async (req, res) => {
+    if (req.body.password) {
+        //encrpting
+        req.body.password = CryptoJS.AES.encrypt(
+            req.body.password,
+            process.env.PASS_SEC
+        ).toString();
+    }
+    try {
+        const updatedUser = await User.findByAndUpdate(req.params.id, {
+            $set: req.body
+        }, { new: true }
+        );
+        res.status(200).json(updatedUser);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router //exporting router
